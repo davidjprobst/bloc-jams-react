72c67212017ae7fb3274f7ccc5ef5a8daedcb52f
David Probst <probst.dj2@gmail.com>
2018-04-22 14:56:54 -0500

    add song list

--- src/components/Album.js
+++ src/components/Album.js
@@ -48,10 +48,10 @@ class Album extends Component {
               <tr className="song" key={index} onClick={ () => this.togglePlaying(index) }>
                 <td id="song-number-column" className="song-action">
                   <span className="song-number">{index + 1}</span>
-                  <span className="song-icon">•</span>
+                  <span className="ion-play"></span>
                 </td>
                 <td id="song-title-column" className="song-title">{song.title}</td>
-                <td id="song-duration-column" className="song-duration">{song.duration}</td>
+                <td id="song-duration-column" className="song-duration">{parseInt(song.duration)}</td>
               </tr>
               )
             }
